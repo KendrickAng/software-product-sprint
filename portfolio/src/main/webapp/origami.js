52342063e764d866cbac8e2e227382edb3d91185
Kendrick Ang <kendrick.ang@ninjavan.co>
2020-06-06 12:40:36 +0800

    add responsive sizing for gallery

--- portfolio/src/main/webapp/origami.js
+++ portfolio/src/main/webapp/origami.js
@@ -11,7 +11,7 @@ function loadOrigamiToGallery() {
         'butterfly', 'mouse', 'penguin', 'pinwheel', 'fish', 'turtle', 'rabbit', 'elephant', 'heart'
     ];
 
-    // create cards for every image. A card is defined to have an <img> element and <span> element
+    // create cards for every image. A card is defined as a <div> with an <img> element and <div> element
     for (let i = 1; i <= N; i++) {
         const card = document.createElement('div');
         card.className = CARD_CN;
@@ -23,11 +23,11 @@ function loadOrigamiToGallery() {
         img.setAttribute('src', src);
         img.setAttribute('alt', 'Origami');
 
-        const span = document.createElement('span');
-        span.innerHTML = origami[i];
+        const div = document.createElement('div');
+        div.innerHTML = origami[i];
 
         card.appendChild(img);
-        card.appendChild(span);
+        card.appendChild(div);
         gallery.appendChild(card);
     }
 }
