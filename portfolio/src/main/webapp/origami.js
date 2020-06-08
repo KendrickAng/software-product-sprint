1bcfb858f675556307f024fb145b3a2f55116622
Kendrick Ang <kendrick.ang@ninjavan.co>
2020-06-09 00:10:56 +0800

    refactor: clearer names, 0-indexing

--- portfolio/src/main/webapp/origami.js
+++ portfolio/src/main/webapp/origami.js
@@ -1,5 +1,5 @@
 // number of images in the origami folder, hardcoded :(
-const N = 20;
+const NUMBER_OF_IMAGES = 20;
 const PATH_PREFIX = 'images/origami/';
 const CARD_CN = 'gallery__card';
 
@@ -7,19 +7,19 @@ function loadOrigamiToGallery() {
     const gallery = document.getElementById('gallery');
     // 1-indexed for easier reference
     const origami = [
-        '', 'bird', 'tulip', 'crab', 'flower', 'frog', 'boat', 'swan', 'raccoon', 'fox', 'dog', 'cat',
+        'bird', 'tulip', 'crab', 'flower', 'frog', 'boat', 'swan', 'raccoon', 'fox', 'dog', 'cat',
         'butterfly', 'mouse', 'penguin', 'pinwheel', 'fish', 'turtle', 'rabbit', 'elephant', 'heart'
     ];
 
     // create cards for every image. A card is defined as a <div> with an <img> element and <div> element
-    for (let i = 1; i <= N; i++) {
+    for (let i = 0; i < NUMBER_OF_IMAGES; i++) {
         const card = document.createElement('div');
         card.className = CARD_CN;
 
         const img = document.createElement('img');
-        const src = i < 10
-            ? `${PATH_PREFIX}00${i}-${origami[i]}.svg`
-            : `${PATH_PREFIX}0${i}-${origami[i]}.svg`;
+        const src = i < 9
+            ? `${PATH_PREFIX}00${i+1}-${origami[i]}.svg`
+            : `${PATH_PREFIX}0${i+1}-${origami[i]}.svg`;
         img.setAttribute('src', src);
         img.setAttribute('alt', 'Origami');
 
