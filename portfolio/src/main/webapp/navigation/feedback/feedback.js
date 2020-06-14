5fd9f4552d0d548b26b9f428473e2af32c04f972
Kendrick Ang <kendrick.ang@ninjavan.co>
2020-06-14 16:21:52 +0800

    refactor and add css styling for comments - move comments to feedback page

--- portfolio/src/main/webapp/navigation/feedback/feedback.js
+++ portfolio/src/main/webapp/navigation/feedback/feedback.js
@@ -1,8 +1,13 @@
+const CLASSNAME_COMMENTS_CONTAINER = 'comments__container';
+const CLASSNAME_COMMENTS_ITEM = 'comments__item';
+const ID_COMMENTS = 'comments';
+const DEFAULT_USER_NAME = 'Anonymous';
+
 // fetches comments from the server and populates the html element with id 'comments'
 async function getComments() {
     const response = await fetch('/comments');
     const comments = await response.json(); // collection of comments
-    const container = document.getElementById('comments');
+    const container = document.getElementById(ID_COMMENTS);
     insertComments(comments, container);
 }
 
@@ -10,11 +15,12 @@ async function getComments() {
 function insertComments(comments, container) {
     console.log(comments);
     const commentsContainer = document.createElement('ul');
-    commentsContainer.className = 'comments__container';
+    commentsContainer.className = CLASSNAME_COMMENTS_CONTAINER;
 
     for (let { name, content, timestamp } of comments) {
-        name = name ? name : 'Anonymous';
+        name = name ? name : DEFAULT_USER_NAME;
         const commentsItem = document.createElement('li');
+        commentsItem.className = CLASSNAME_COMMENTS_ITEM;
         const date = new Date(timestamp).toDateString();
         commentsItem.innerText = `${content} - ${name}, ${date}`;
         commentsContainer.appendChild(commentsItem);
